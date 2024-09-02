210a9ece1dcdd093a3b25862abf83828f95c000c
Stervar <150718158+Stervar@users.noreply.github.com>
2024-09-02 22:30:55 +0300

    def encode(input_string):       count = 1       prev = ''       lst = []       for character in input_string:           if character != prev:               if prev:                   entry = (prev, count)                   lst.append(entry)               count = 1               prev = character           else:               count += 1       else:           entry = (character, count)           lst.append(entry)       return lst
      def decode(lst):
          q = ''
          for character, count in lst:
              q += character * count
          return q

---
+++ PythonHypothesis.py
@@ -0,0 +1,25 @@
+# Пример:
+
+def encode(input_string):
+    count = 1
+    prev = ''
+    lst = []
+    for character in input_string:
+        if character != prev:
+            if prev:
+                entry = (prev, count)
+                lst.append(entry)
+            count = 1
+            prev = character
+        else:
+            count += 1
+    else:
+        entry = (character, count)
+        lst.append(entry)
+    return lst
+
+def decode(lst):
+    q = ''
+    for character, count in lst:
+          q += character * count
+    return q
